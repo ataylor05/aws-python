2b4bc2660249fb04a773d56f64ed6af107b7c595
Allan Taylor <allan@startmail.com>
2021-09-28 14:18:20 -0400

    Update iamFunctions.py

--- iamFunctions.py
+++ iamFunctions.py
@@ -2,22 +2,31 @@ import boto3
 
 client = boto3.client('iam')
 
-def create_role():
-    response = client.create_role(
-        Path='string',
-        RoleName='string',
-        AssumeRolePolicyDocument='string',
-        Description='string',
-        MaxSessionDuration=123,
-        PermissionsBoundary='string',
-        Tags=[
-            {
-                'Key': 'string',
-                'Value': 'string'
-            },
-        ]
-    )
-
+def create_role(name, trust_policy, description="", permissions_boundary=None, path="/", max_session_duration=3600, tags=[]):
+    try:
+        if permissions_boundary == None:
+            response = client.create_role(
+                Path=path,
+                RoleName=name,
+                AssumeRolePolicyDocument=trust_policy,
+                Description=description,
+                MaxSessionDuration=max_session_duration,
+                Tags=tags
+            )
+            return response["Role"]
+        else:
+            response = client.create_role(
+                Path=path,
+                RoleName=name,
+                AssumeRolePolicyDocument=trust_policy,
+                Description=description,
+                MaxSessionDuration=max_session_duration,
+                PermissionsBoundary=permissions_boundary,
+                Tags=tags
+            )
+            return response["Role"]
+    except Exception as error: print(error)
+    
 def create_group(group_name, path="/"):
     try:
         response = client.create_group(
@@ -71,4 +80,25 @@ def attach_policy_to_group(group_name, policy_arn):
         return "Policy has been attached to group"
     except Exception as error: print(error)
 
+def attach_policy_to_role(role_name, policy_arn):
+    try:
+        client.attach_role_policy(
+            RoleName=role_name,
+            PolicyArn=policy_arn
+        )
+        return "Policy has been attached to role"
+    except Exception as error: print(error)
 
+def create_policy(name, document, description="", path="/", tags=[]):
+    try:
+        response = client.create_policy(
+            PolicyName=name,
+            Path=path,
+            PolicyDocument=document,
+            Description=description,
+            Tags=tags
+        )
+        return response["Policy"]
+    except Exception as error: print(error)
+
+    
